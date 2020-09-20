ebdbe91a67562a16b2ec47f4687136778f6869d8
Brandon Jones <bcjones1@fullsail.edu>
2020-09-20 13:34:42 -0400

    Add new ABP for base Movement, start implementing Mission System
    Added new ABP from MoCap Basics
    Started implementing Mission Component framework

---
+++ Source/ProjectAurora/System/ProjectAuroraGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "ProjectAuroraGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTAURORA_API UProjectAuroraGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
